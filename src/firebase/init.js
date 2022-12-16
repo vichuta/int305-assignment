68ba617159c9d4731c4e8da997ec04385d25b61b
vichuta <vichuta.nice@kmutt.ac.th>
2022-12-16 22:59:30 +0700

    Project Assignment complete

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -6,12 +6,7 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoX-ni4vp-MfMRr3KWBfnnrPhyPt7msYQ",
-  authDomain: "int305fb106.firebaseapp.com",
-  projectId: "int305fb106",
-  storageBucket: "int305fb106.appspot.com",
-  messagingSenderId: "149014982304",
-  appId: "1:149014982304:web:d7578104e2eeea10a6a3c5"
+
 };
 
 // Initialize Firebase
